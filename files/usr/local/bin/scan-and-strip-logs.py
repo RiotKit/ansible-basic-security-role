9e3e5c3e6322883c45cac1f52bef17380978e81a
Andrew Johnson <blackandred@users.noreply.github.com>
2020-08-29 12:04:16 +0200

    Fix IPv4 stripping - handle decoding errors, add XZ support, scan all files (not only *.log and *.gz - as logs sometimes have different patterns like log.1, log.2)

--- files/usr/local/bin/scan-and-strip-logs.py
+++ files/usr/local/bin/scan-and-strip-logs.py
@@ -1,24 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import sys
 
 
-def find_files(files, dirs=[], extensions=[]):
+def find_files(files, dirs: list = []):
     new_dirs = []
     for d in dirs:
         try:
             new_dirs += [ os.path.join(d, f) for f in os.listdir(d) ]
         except OSError:
-            if os.path.splitext(d)[1] in extensions:
-                files.append(d)
+            files.append(d)
 
     if new_dirs:
-        find_files(files, new_dirs, extensions )
+        find_files(files, new_dirs)
 
 
 logs_files = []
-find_files(logs_files, dirs=['/var/log'], extensions=['.log', '.gz'])
+find_files(logs_files, dirs=['/var/log'])
 
 for file_path in logs_files:
     print("Processing " + file_path)
@@ -27,8 +26,21 @@ for file_path in logs_files:
         print(file_path + ' needs to be writable')
         sys.exit(1)
 
-    if ".gz" in file_path:
-        os.system("cat " + file_path + " | gunzip | strip-ipv4-address.py | gzip -f > /tmp/.log.cleanup && mv /tmp/.log.cleanup " + file_path)
+    if file_path.endswith('.gz'):
+        os.system(
+            'zcat "{file_path}" | strip-ipv4-address.py | gzip -f > /tmp/.log.cleanup && mv /tmp/.log.cleanup "{file_path}"'
+            .format(file_path=file_path)
+        )
         continue
 
-    os.system("cat " + file_path + " | strip-ipv4-address.py > /tmp/.log.cleanup && mv /tmp/.log.cleanup " + file_path)
+    if file_path.endswith('.xz'):
+        os.system(
+            'xzcat "{file_path}" | strip-ipv4-address.py | xz -f > /tmp/.log.cleanup && mv /tmp/.log.cleanup "{file_path}"'
+            .format(file_path=file_path)
+        )
+        continue
+
+    os.system(
+        'cat "{file_path}" | strip-ipv4-address.py > /tmp/.log.cleanup && mv /tmp/.log.cleanup "{file_path}"'
+        .format(file_path=file_path)
+    )
--- files/usr/local/bin/strip-ipv4-address.py
+++ files/usr/local/bin/strip-ipv4-address.py
@@ -1,9 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import re
 import sys
 
-ip_addr_regex = re.compile(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b')
-replaced = re.sub(ip_addr_regex, '1.3.1.2', sys.stdin.read())
+line = sys.stdin.read()
+
+try:
+    ip_addr_regex = re.compile(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b')
+    replaced = re.sub(ip_addr_regex, '1.3.1.2', line)
+except UnicodeDecodeError:
+    print(line)
+    sys.exit(1)
 
 print(replaced)
